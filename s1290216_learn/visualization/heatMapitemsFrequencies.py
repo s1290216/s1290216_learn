f77ceef8b4bec9645610c647b74f32cdb2175e4c
s1290216 <s1290216@u-aizu.ac.jp>
2023-07-27 16:50:59 +0900

    temporarily saved

--- s1290216_learn/visualization/heatMapitemsFrequencies.py
+++ s1290216_learn/visualization/heatMapitemsFrequencies.py
@@ -5,21 +5,25 @@ class heatMapItemsFrequencies:
         latitude = []
         longitude = []
         frequency = []
-        point = []
         for k, v in itemsFrequenciesDictionary.items():
-            point.append(k.replace('[\'', '').replace('\']'))
-            long, lati = k.replace('[\'Point(', '').replace(')\']', '').split()
-            latitude.append(float(lati))
-            longitude.append(float(long))
-            frequency.append(int(v))
-        figure = px.scatter_mapbox(
+            if ',' in k:
+                points = k.split(',')
+                for p in points:
+                    long, lati = p.replace('Point(', '').replace(').1', '').replace(')', '').split()
+                    latitude.append(float(lati))
+                    longitude.append(float(long))
+                    frequency.append(int(v))
+            else:
+                long, lati = k.replace('[\'Point(', '').replace(')\']', '').replace(').1\']', '').split()
+                latitude.append(float(lati))
+                longitude.append(float(long))
+                frequency.append(int(v))
+        figure = px.density_mapbox(
             lat = latitude,
             lon = longitude,
-            hover_name = point,
-            color = frequency,
+            z = frequency,
             size = frequency,
-            size_max = 30,
-            opacity = 0.4,
+            radius = 10,
             center = {'lat': 34.686567, 'lon': 135.52000},
             zoom = 4,
             height = 600,
@@ -29,3 +33,6 @@ class heatMapItemsFrequencies:
         figure.update_layout(margin = {"r": 0, "t": 0, "l": 0, "b": 0})
         figure.update_layout(title_text = "HeatMap")
         figure.show()
+
+if __name__ == "__main__":
+    heatMapItemsFrequencies(dictionary)
